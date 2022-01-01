f4774957d62770003aac627869bcfe043d0121c9
tedo0627 <tedosw1mpr1ver@gmail.com>
2022-01-02 03:20:09 +0900

    Add button

--- src/tedo0627/redstonecircuit/RedstoneCircuit.php
+++ src/tedo0627/redstonecircuit/RedstoneCircuit.php
@@ -12,6 +12,8 @@ use tedo0627\redstonecircuit\block\mechanism\BlockRedstoneLamp;
 use tedo0627\redstonecircuit\block\power\BlockLever;
 use tedo0627\redstonecircuit\block\power\BlockRedstone;
 use tedo0627\redstonecircuit\block\power\BlockRedstoneTorch;
+use tedo0627\redstonecircuit\block\power\BlockStoneButton;
+use tedo0627\redstonecircuit\block\power\BlockWoodenButton;
 use tedo0627\redstonecircuit\block\transmission\BlockRedstoneComparator;
 use tedo0627\redstonecircuit\block\transmission\BlockRedstoneRepeater;
 use tedo0627\redstonecircuit\block\transmission\BlockRedstoneWire;
@@ -20,13 +22,26 @@ use tedo0627\redstonecircuit\item\ItemRedstone;
 class RedstoneCircuit extends PluginBase {
 
     public function onLoad(): void {
-        $this->registerBlock(Ids::REDSTONE_WIRE, fn($bid, $name, $info) => new BlockRedstoneWire($bid, $name, $info));
-        $this->registerBlock(Ids::REDSTONE_BLOCK, fn($bid, $name, $info) => new BlockRedstone($bid, $name, $info));
+        // mechanism
         $this->registerBlock(Ids::REDSTONE_LAMP, fn($bid, $name, $info) => new BlockRedstoneLamp($bid, $name, $info));
+
+        // power
         $this->registerBlock(Ids::LEVER, fn($bid, $name, $info) => new BlockLever($bid, $name, $info));
-        $this->registerBlock(Ids::UNPOWERED_REPEATER, fn($bid, $name, $info) => new BlockRedstoneRepeater($bid, $name, $info));
-        $this->registerBlock(Ids::UNPOWERED_COMPARATOR, fn($bid, $name, $info) => new BlockRedstoneComparator($bid, $name, $info));
+        $this->registerBlock(Ids::REDSTONE_BLOCK, fn($bid, $name, $info) => new BlockRedstone($bid, $name, $info));
         $this->registerBlock(Ids::REDSTONE_TORCH, fn($bid, $name, $info) => new BlockRedstoneTorch($bid, $name, $info));
+        $this->registerBlock(Ids::STONE_BUTTON, fn($bid, $name, $info) => new BlockStoneButton($bid, $name, $info));
+        $this->registerBlock(Ids::WOODEN_BUTTON, fn($bid, $name, $info) => new BlockWoodenButton($bid, $name, $info));
+        $this->registerBlock(Ids::ACACIA_BUTTON, fn($bid, $name, $info) => new BlockWoodenButton($bid, $name, $info));
+        $this->registerBlock(Ids::BIRCH_BUTTON, fn($bid, $name, $info) => new BlockWoodenButton($bid, $name, $info));
+        $this->registerBlock(Ids::DARK_OAK_BUTTON, fn($bid, $name, $info) => new BlockWoodenButton($bid, $name, $info));
+        $this->registerBlock(Ids::JUNGLE_BUTTON, fn($bid, $name, $info) => new BlockWoodenButton($bid, $name, $info));
+        $this->registerBlock(Ids::SPRUCE_BUTTON, fn($bid, $name, $info) => new BlockWoodenButton($bid, $name, $info));
+
+        // transmission
+        $this->registerBlock(Ids::UNPOWERED_COMPARATOR, fn($bid, $name, $info) => new BlockRedstoneComparator($bid, $name, $info));
+        $this->registerBlock(Ids::UNPOWERED_REPEATER, fn($bid, $name, $info) => new BlockRedstoneRepeater($bid, $name, $info));
+        $this->registerBlock(Ids::REDSTONE_WIRE, fn($bid, $name, $info) => new BlockRedstoneWire($bid, $name, $info));
+
         ItemFactory::getInstance()->register(new ItemRedstone(new ItemIdentifier(ItemIds::REDSTONE, 0), "Redstone"), true);
     }
 
--- src/tedo0627/redstonecircuit/block/power/BlockStoneButton.php
+++ src/tedo0627/redstonecircuit/block/power/BlockStoneButton.php
@@ -3,7 +3,7 @@
 namespace tedo0627\redstonecircuit\block\power;
 
 use pocketmine\block\Block;
-use pocketmine\block\Lever;
+use pocketmine\block\StoneButton;
 use pocketmine\item\Item;
 use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
@@ -16,7 +16,7 @@ use tedo0627\redstonecircuit\block\IRedstoneComponent;
 use tedo0627\redstonecircuit\block\LinkRedstoneWireTrait;
 use tedo0627\redstonecircuit\block\RedstoneComponentTrait;
 
-class BlockLever extends Lever implements IRedstoneComponent, ILinkRedstoneWire {
+class BlockStoneButton extends StoneButton implements IRedstoneComponent, ILinkRedstoneWire {
     use LinkRedstoneWireTrait;
     use RedstoneComponentTrait;
 
@@ -25,33 +25,42 @@ class BlockLever extends Lever implements IRedstoneComponent, ILinkRedstoneWire 
         return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
     }
 
-    public function onBreak(Item $item, ?Player $player = null): bool {
-        parent::onBreak($item, $player);
-        BlockUpdateHelper::updateAroundDirectionRedstone($this, Facing::opposite($this->getFacing()->getFacing()));
-        return true;
-    }
-
     public function onNearbyBlockChange(): void {
-        if (FlowablePlaceHelper::checkSurface($this, Facing::opposite($this->getFacing()->getFacing()))) return;
+        if (FlowablePlaceHelper::checkSurface($this, Facing::opposite($this->getFacing()))) return;
         $this->getPosition()->getWorld()->useBreakOn($this->getPosition());
     }
 
     public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null): bool {
+        if ($this->isPressed()) return true;
+
         parent::onInteract($item, $face, $clickVector, $player);
-        BlockUpdateHelper::updateAroundDirectionRedstone($this, Facing::opposite($this->getFacing()->getFacing()));
+        BlockUpdateHelper::updateAroundDirectionRedstone($this, Facing::opposite($this->getFacing()));
+        return true;
+    }
+
+    public function onScheduledUpdate(): void {
+        if (!$this->isPressed()) return;
+
+        parent::onScheduledUpdate();
+        BlockUpdateHelper::updateAroundDirectionRedstone($this, Facing::opposite($this->getFacing()));
+    }
+
+    public function onBreak(Item $item, ?Player $player = null): bool {
+        parent::onBreak($item, $player);
+        if ($this->isPressed()) BlockUpdateHelper::updateAroundDirectionRedstone($this, Facing::opposite($this->getFacing()));
         return true;
     }
 
     public function getStrongPower(int $face): int {
-        if (!$this->isActivated()) return 0;
-        return $face === $this->getFacing()->getFacing() ? 15 : 0;
+        if (!$this->isPressed()) return 0;
+        return $face === $this->getFacing() ? 15 : 0;
     }
 
     public function getWeakPower(int $face): int {
-        return $this->isActivated() ? 15 : 0;
+        return $this->isPressed() ? 15 : 0;
     }
 
     public function isPowerSource(): bool {
-        return $this->isActivated();
+        return $this->isPressed();
     }
 }

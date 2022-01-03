528233f2ecf22ba7763ddcb5413040fc71b15776
tedo0627 <tedosw1mpr1ver@gmail.com>
2022-01-03 12:48:58 +0900

    Fix not set powered when place door

--- src/tedo0627/redstonecircuit/block/mechanism/BlockIronDoor.php
+++ src/tedo0627/redstonecircuit/block/mechanism/BlockIronDoor.php
@@ -2,11 +2,13 @@
 
 namespace tedo0627\redstonecircuit\block\mechanism;
 
+use pocketmine\block\Block;
 use pocketmine\block\Door;
 use pocketmine\item\Item;
 use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
 use pocketmine\player\Player;
+use pocketmine\world\BlockTransaction;
 use pocketmine\world\sound\DoorSound;
 use tedo0627\redstonecircuit\block\BlockPowerHelper;
 use tedo0627\redstonecircuit\block\IRedstoneComponent;
@@ -14,6 +16,13 @@ use tedo0627\redstonecircuit\block\RedstoneComponentTrait;
 
 class BlockIronDoor extends Door implements IRedstoneComponent {
     use RedstoneComponentTrait;
+
+    public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null): bool {
+        $other = $this->getSide(Facing::UP);
+        $powered = BlockPowerHelper::isPowered($this) || BlockPowerHelper::isPowered($other);
+        $this->setPowered($powered);
+        return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
+    }
 
     public function onInteract(Item $item, int $face, Vector3 $clickVector, ?Player $player = null): bool {
         return false;
--- src/tedo0627/redstonecircuit/block/mechanism/BlockWoodenDoor.php
+++ src/tedo0627/redstonecircuit/block/mechanism/BlockWoodenDoor.php
@@ -2,9 +2,14 @@
 
 namespace tedo0627\redstonecircuit\block\mechanism;
 
+use pocketmine\block\Block;
 use pocketmine\block\Door;
 use pocketmine\block\WoodenDoor;
+use pocketmine\item\Item;
 use pocketmine\math\Facing;
+use pocketmine\math\Vector3;
+use pocketmine\player\Player;
+use pocketmine\world\BlockTransaction;
 use pocketmine\world\sound\DoorSound;
 use tedo0627\redstonecircuit\block\BlockPowerHelper;
 use tedo0627\redstonecircuit\block\IRedstoneComponent;
@@ -12,6 +17,13 @@ use tedo0627\redstonecircuit\block\RedstoneComponentTrait;
 
 class BlockWoodenDoor extends WoodenDoor implements IRedstoneComponent {
     use RedstoneComponentTrait;
+
+    public function place(BlockTransaction $tx, Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, ?Player $player = null): bool {
+        $other = $this->getSide(Facing::UP);
+        $powered = BlockPowerHelper::isPowered($this) || BlockPowerHelper::isPowered($other);
+        $this->setPowered($powered);
+        return parent::place($tx, $item, $blockReplace, $blockClicked, $face, $clickVector, $player);
+    }
 
     public function onRedstoneUpdate(): void {
         $other = $this->getSide($this->isTop() ? Facing::DOWN : Facing::UP);

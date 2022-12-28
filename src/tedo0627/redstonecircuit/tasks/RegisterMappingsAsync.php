be6be2d83b0d83ba5705a9e6a17cfb7bd9546f6f
Florian <38558911+xxFLORII@users.noreply.github.com>
2022-12-28 13:05:36 +0100

    remove useless spaces

--- src/tedo0627/redstonecircuit/tasks/RegisterMappingsAsync.php
+++ src/tedo0627/redstonecircuit/tasks/RegisterMappingsAsync.php
@@ -6,8 +6,6 @@ use pocketmine\scheduler\AsyncTask;
 use tedo0627\redstonecircuit\RedstoneCircuit;
 
 class RegisterMappingsAsync extends AsyncTask {
-
-
     public function onRun(): void {
         RedstoneCircuit::registerMappings();
     }
